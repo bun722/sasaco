5281e633e28d2c34b967a06614a1a7ccc6c10d77
bun722 <u.t.m.bun@gmail.com>
2020-01-04 22:07:00 +0900

    ユーザー情報更新時に現在のパスワードの入力を不要にしつつ、パスワードの変更は可能になるよう編集(SNSでの登録の場合はパスワード自体不要にした)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'registrations'
+  }
   resources :home, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
