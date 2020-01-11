698a4029cd49f557f305ed29ae88a437aeb55304
bun722 <u.t.m.bun@gmail.com>
2020-01-11 15:50:30 +0900

    重複していたルーティングを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/new'
-  get 'posts/show'
-
   root 'home#index'
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'registrations'
   }
   resources :home, only: [:index]
-  resources :posts, only: [:index, :show, :create]
+  resources :posts, only: [:index, :new, :show, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
